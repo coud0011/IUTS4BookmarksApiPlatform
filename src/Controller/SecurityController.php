fa02511c3db95b264bac8765f21b7876c4dc5463
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2024-02-14 13:44:47 +0100

    Réparation des coquilles pour l'authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,7 +13,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('app_doc');
+            return $this->redirectToRoute('api_doc');
         }
 
         // get the login error if there is one
